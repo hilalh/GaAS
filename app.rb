890e7c153242f3260282dd0c83b8a96c37ec0b62
Hilal Habashi <habashi2@illinois.edu>
2014-10-24 17:46:31 -0500

    Developed more advanced hierarchy

--- app.rb
+++ app.rb
@@ -5,26 +5,43 @@ require 'soundcloud'
 require 'sinatra'
 
 def search_by_lyrics(query, number)
-	strings = RapGenius.search_by_lyrics(query)[0..2].collect do | song |
+	strings = RapGenius.search_by_lyrics(query)[0..3].collect do | song |
 		song.title + " - " + song.artist.name
 	end
 	strings.join("\n")
 end
 
 def search_by_title(query, number)
-	strings = RapGenius.search_by_title(query)[0..5].collect do | song |
+	songs = RapGenius.search_by_title(query)[0..10].collect do | song |
 		puts song
 	end
+    puts songs
 end
 
+def parse_song_list(songsCollection){
+    songsCollection.each { |song| puts #{}song.  }
+}
 
-def getLyrics(query)
-  song = RapGenius.search_by_title(query)[0]
+def rap_genius_from_name(query, number)
+	rapgenius_object_id = RapGenius.search_by_lyrics(query)[0]["id"]
+	"genius.com/songs/" + id
+end
+
+bas
+
+def getLyrics(songId)
   puts song.title
-  iterate_lyrics(song)
+  iterate_lines(song)
 end
 
-def concat_lyrics(buffer,current)
+def iterate_lines(song)
+  output=""
+  current_lines = song.lines
+  current_lines.each{|current| concat_lines(output,current)}
+  puts result_lyrics
+end
+
+def concat_lines(buffer,current)
   if(current.lyric[0] == ",")
     buffer << "\n\n"
   else
@@ -32,24 +49,21 @@ def concat_lyrics(buffer,current)
   end
 end
 
-def iterate_lyrics(song)
-  result_lyrics=""
-  current_lines = song.lines
-  current_lines.each{|current| concat_lyrics(result_lyrics,current)}
-  puts result_lyrics
-end
-
 
 get '/' do
-    'Nothin\' here homie'
+    'Genius-As-A-Service\n------------------\n\n
+    Choose your poison mane
+\n\n
+    Seriously though.\n
+    Currently we\'re offering querying service only.\n
+    You could query by song, by lyrics or by artist'
 end
 
-get '/lyrics/:name' do
-    songName = params['name']
-    "#{getLyrics(songName)}"
+#Print name of artist, name of song, release year, album
+get '/:songId/' do
+    current = params['songId']
+    "#{print_details(current)}"
 end
 
-def rap_genius_from_name(query, number)
-	rapgenius_object_id = RapGenius.search_by_lyrics(query)[0]["id"]
-	"genius.com/songs/" + id
-end
+
+
